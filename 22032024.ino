d463a7360768f78fd5c690f52a02d4029c98177e
SiemieLniane <mkmkolodziejski@gmail.com>
2024-03-22 20:56:51 +0100

    Servo obraca się do przodu i do tyłu

--- 22032024.ino
+++ 22032024.ino
@@ -1,67 +1,24 @@
-// These constants won't change. They're used to give names to the pins used:
+#include<Servo.h>
 
+const int servoPin = 2;
 
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+Servo servo;
 
-
-const int digitalOutPin1 = 9; // Analog output pin that the LED is attached to
-const int digitalOutPin2 = 8; // Analog output pin that the LED is attached to
-const int digitalOutPin3 = 7; // Analog output pin that the LED is attached to
-
-
-int sensorValue = 0;        // value read from the pot
-
-int outputValue = 0;        // value output to the PWM (analog out)
-
+int servoAngle = 0;
 
 void setup() {
-
-
-  // initialize serial communications at 9600 bps:
-
-
-  pinMode(digitalOutPin1, OUTPUT);
-  pinMode(digitalOutPin2, OUTPUT);
-  pinMode(digitalOutPin3, OUTPUT);
+  Serial.begin(9600);
+  servo.attach(servoPin);
 }
 
-
 void loop() {
-
-
-  // read the analog in value:
-
-
-  sensorValue = analogRead(analogInPin);
-
-
-  // map it to the range of the analog out:
-
-
-  outputValue = map(sensorValue, 0, 1023, 0, 255);
-
-
-  // change the analog out value:
-  int digitalOut1 = LOW;
-  int digitalOut2 = LOW;
-  int digitalOut3 = LOW;
-  if (outputValue > 0 && outputValue < 80) {
-
-    digitalOut1 = HIGH;
-
-  }else if (outputValue <160) {
-    
-    digitalOut2 = HIGH;
-
-  }else {
-    
-    digitalOut3 = HIGH;
-
+  for (int i = 0; i <= 180; i++) {
+    servo.write(i);
+    delay(10);
   }
-
-    digitalWrite(digitalOutPin1,digitalOut1);
-    digitalWrite(digitalOutPin2,digitalOut2);
-    digitalWrite(digitalOutPin3,digitalOut3);
-
-
+  
+  for (int i = 180; i >= 0; i--) {
+    servo.write(i);
+    delay(10);
+  }
 }
